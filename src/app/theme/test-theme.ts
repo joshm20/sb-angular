8adef4944dc4e66a6864d5cc2659d8e00a47c6af
Josh Makela <business0702joshm@yahoo.com>
2024-01-23 18:32:56 -0500

    Playing with test-theme, trying to make settings box go with theme

--- src/app/theme/test-theme.ts
+++ src/app/theme/test-theme.ts
@@ -13,6 +13,13 @@ let slate = '#333333';
 let darkgray = '#595959';
 let gray = '#B2B2B2';
 let lightgray = '#D6D6D6';
+let black = '#000000';
+
+let darkNavy = '#27374D';
+let lightNavy = '#526D82';
+let gray2 = '#798082';
+let babyBlue = '#b5dde8';
+let offwhiteBlue = '#DDE6ED';
 
 let darkgreen = '#5F791C';
 let green = '#ADCC23';
@@ -23,12 +30,16 @@ let blue = '#5498B6';
 let lightblue = '#D2EBF5';
 
 let red = '#A4232B';
+let brightred = '#FF0000';
+let rubyred = '#E0115F';
 let orange = '#FFBD00';
+let yellow = '#e8eb34';
+let pink = '#FF00FF';
 
 export const testTheme: Theme = {
   name: 'test',
   fonts: "https://fonts.googleapis.com/css2?family=Lato&family=Yellowtail&display=swap",
-  properties: {
+  /*properties: {
     '--background': slate,
     '--on-background': orange,
 
@@ -79,5 +90,91 @@ export const testTheme: Theme = {
     '--text-font': 'Lato, sans-serif',
     '--menu-font': 'Lato, sans-serif',
     '--user-font': '"Roboto Mono", monospace'
+  } */
+
+  properties: {
+    '--background': white,
+    '--on-background': black,
+
+    '--header-background': gray,
+    '--on-header-background': white,
+
+    '--footer-background': gray,
+    '--on-footer-background': white,
+
+    '--default-icon-color': orange,
+
+    '--icon-button-background': white,
+    '--on-icon-button-background': darkNavy,
+
+    '--card-shadow': lightblue,
+    '--button-shadow': darkblue,
+
+    '--card-border': red,
+    '--card-border-hover': white,
+    '--button-border': yellow,
+
+    '--link': blue,
+    '--link-active': blue,
+    '--link-hover': blue,
+    '--link-visited': blue,
+
+    '--background-hover': white,
+
+    '--go-button-background': lightgreen,
+    '--on-go-button-background': green,
+    '--cancel-button-background': darkgreen,
+    '--on-cancel-button-background': lightgreen,
+    '--danger-button-background': red,
+    '--on-danger-button-background': green,
+    '--disabled-button-background': darkgreen,
+    '--on-disabled-button-background': lightgreen,
+
+    '--nav-item-selected-background': red,
+    '--on-nav-item-selected-background': brightred,
+    '--nav-item-hover-background': rubyred,
+    '--on-nav-item-hover-background': brightred,
+
+    '--spinner': darkNavy,
+    '--progress-bar-element': gray,
+    '--progress-bar-fill': darkNavy,
+
+    '--scrollbar-track': white,
+    '--scrollbar': darkNavy,
+    '--scrollbar-thumb': darkNavy,
+
+    '--drag-placeholder-background': lightgray, // Transparent box that appears when reorganizing epics/issues
+    '--drag-placeholder-border': white,
+
+    '--arrow-background': red, //white,
+    '--on-arrow-background':red, //darkNavy,
+
+    '--selected-state-button-background': red, //darkNavy,
+    '--on-selected-state-button-background': red,//white,
+    '--unselected-state-button-icon': red,//darkNavy,
+    '--issue-state-card-background': red,//lightNavy,
+
+    '--list-selected': darkNavy,
+
+    '--form-field-underline': white,
+    '--form-field-caret': white,
+    '--form-field-fill': orange,
+    '--form-select-color': white,
+
+    '--mat-select-value-text-color': darkNavy,
+
+    '--error': red,
+    '--on-error': white,
+    '--okay': darkgreen,
+    '--on-okay': white,
+
+    '--warning': orange,
+    '--on-warning': darkNavy,
+
+    '--logo-font': 'Helvetica, sans-serif',
+    '--banner-font': 'Helvetica, sans-serif',
+    '--text-font': 'Consolas, Courier, monospace',
+    '--menu-font': 'Helvetica, sans-serif',
+    '--user-font': 'Consolas, Courier, monospace'
   }
 };
