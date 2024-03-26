ba77d83d06b497d356458c5d5654cc1213a383ff
Nate5524 <natebaker757@gmail.com>
2024-03-26 17:09:55 -0400

    created component

---
+++ src/app/create-project/create-project.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'sb-create-project',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './create-project.component.html',
+  styleUrl: './create-project.component.css'
+})
+export class CreateProjectComponent {
+
+}
